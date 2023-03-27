b83dc4d5f403d03f3a69412f33188ee97c029581
pkwame <45004664+pkwame@users.noreply.github.com>
2023-03-27 18:11:48 +0100

    extend pvc

--- environment/daily-automation-tmpcleanup/docker/script/copy-notebooks.sh
+++ environment/daily-automation-tmpcleanup/docker/script/copy-notebooks.sh
@@ -5,22 +5,9 @@ pod_name=$(oc get pod dev-jupyter-data-science --output=name | sed -e 's/^[[pod/
 oc exec $pod_name -- bash -c '
         echo "Establishing connection to '$pod_name'"
         sleep 5
+        find /notebooks-pv/* -mtime +1 -exec rm {} \;
         echo "Established connection to '$pod_name'"
         pwd
         mkdir /notebooks-pv/"$(date +"%d-%m-%Y")"
-
         cp -r /home/notebookuser /notebooks-pv/"$(date +"%d-%m-%Y")"/
-
-        find /notebooks-pv/* -mtime +1 -exec rm {} \;
 '
-
-
-
-
-
-
-
-
-
-
-
