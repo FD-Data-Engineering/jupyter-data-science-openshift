c542b066fec98e122e433bd4cd7d3adb5104faa8
pkwame <45004664+pkwame@users.noreply.github.com>
2023-03-29 12:15:30 +0100

    add restore job

---
+++ environment/daily-automation-tmpcleanup/docker/script/restore-notebooks.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+pod_name=$(oc get pod dev-jupyter-data-science --output=name | sed -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//')
+date=$(date --date "-0 days" +"%d-%m-%Y")
+oc exec $pod_name -- bash -c '
+        echo "Establishing connection to '$pod_name'"
+        sleep 5
+        echo "Established connection to '$pod_name'"
+        echo "Restoring notebooks for '$date' dir"
+        cp -r /notebooks-pv/'$date' /home/notebookuser/
+        echo "notebooks have been copied successfully"
+'
