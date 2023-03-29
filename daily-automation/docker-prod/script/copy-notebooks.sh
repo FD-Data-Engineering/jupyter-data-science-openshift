ed74eb357d7b0314164c7a1fc48c910bdbafff1b
pkwame <45004664+pkwame@users.noreply.github.com>
2023-03-29 17:22:27 +0100

    add prod config

---
+++ daily-automation/docker-prod/script/copy-notebooks.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+pod_name=$(oc get pod prod-jupyter-data-science --output=name | sed -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//')
+date=$(date --date "-2 days" +"%d-%m-%Y")
+oc exec $pod_name -- bash -c '
+        echo "Establishing connection to '$pod_name'"
+        sleep 5
+        echo "deleting notebooks in '$date' dir"
+        rm -rf /notebooks-pv/'$date'
+        echo "Established connection to '$pod_name'"
+        pwd
+        mkdir /notebooks-pv/"$(date +"%d-%m-%Y")"
+        cp -r /home/notebookuser /notebooks-pv/"$(date +"%d-%m-%Y")"/
+        echo "notebooks have been copied successfully to notebooks-pv"
+'
+
+
