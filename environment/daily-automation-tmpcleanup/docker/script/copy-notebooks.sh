4b936a6006ce9a3e3a6ee1f2cf4e0afc7f543c18
pkwame <45004664+pkwame@users.noreply.github.com>
2023-03-29 10:48:46 +0100

    update cleanup script

--- environment/daily-automation-tmpcleanup/docker/script/copy-notebooks.sh
+++ environment/daily-automation-tmpcleanup/docker/script/copy-notebooks.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 #
 pod_name=$(oc get pod dev-jupyter-data-science --output=name | sed -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//')
-
+date=$(date --date "-2 days" +"%d-%m-%Y")
 oc exec $pod_name -- bash -c '
         echo "Establishing connection to '$pod_name'"
         sleep 5
-        find /notebooks-pv/* -mtime +1 -exec rm {} \;
+        echo "deleting notebooks in '$date' dir"
+        rm -rf /notebooks-pv/'$date'
         echo "Established connection to '$pod_name'"
         pwd
         mkdir /notebooks-pv/"$(date +"%d-%m-%Y")"
         cp -r /home/notebookuser /notebooks-pv/"$(date +"%d-%m-%Y")"/
+        echo "notebooks have been copied successfully to notebooks-pv"
 '
+
+
